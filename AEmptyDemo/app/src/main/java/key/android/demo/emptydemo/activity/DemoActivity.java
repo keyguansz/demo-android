eb020a1eea68cfbd6f9aa40ba97d1b306eed3536
key.guan <key.guan@dji.com>
2017-02-24 22:44:26 +0800

    project template~

---
+++ AEmptyDemo/app/src/main/java/key/android/demo/emptydemo/activity/DemoActivity.java
@@ -0,0 +1,18 @@
+package key.android.demo.emptydemo.activity;
+
+import android.os.Bundle;
+
+import key.android.demo.databindingdemo.R;
+
+
+public class DemoActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_demo);
+
+    }
+}
