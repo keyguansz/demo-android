a8c7d9c5b38ee8742ad323319e47908d81a7d741
key.guan <key.guan@dji.com>
2017-04-18 17:56:50 +0800

    goodename local，test Origin

---
+++ FitDemo/app/src/main/java/key/android/demo/FitDemo/activity/ZsTestActivity.java
@@ -0,0 +1,15 @@
+package key.android.demo.FitDemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import key.android.demo.fitdemo.R;
+
+public class ZsTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zs_test);
+    }
+}
