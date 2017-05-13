188ae556c092d701d94a08311174f8188f1d18c0
key.guan <key.guan@dji.com>
2017-05-13 16:58:11 +0800

    project template~

--- FitDemo/app/src/main/java/key/android/demo/FitDemo/activity/ZsTestActivity.java
+++ FitDemo/app/src/main/java/key/android/demo/FitDemo/activity/ZsTestActivity.java
@@ -3,8 +3,13 @@ package key.android.demo.FitDemo.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
-import key.android.demo.fitdemo.R;
+import key.android.demo.FitDemo.R;
 
+/**
+ *@desc
+ *@ref:zs600b layout：large>land>..
+ *@author : key.guan @ 2017/4/18 18:02
+ */
 public class ZsTestActivity extends Activity {
 
     @Override
