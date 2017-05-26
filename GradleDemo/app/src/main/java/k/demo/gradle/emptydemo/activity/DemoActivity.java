290bfb1907a2222c4ae28b257c3c9db03b415d23
key.guan <key.guan@dji.com>
2017-05-26 09:41:14 +0800

    none

---
+++ GradleDemo/app/src/main/java/k/demo/gradle/emptydemo/activity/DemoActivity.java
@@ -0,0 +1,19 @@
+package key.android.demo.emptydemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import key.android.demo.emptydemo.R;
+
+
+public class DemoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_demo);
+
+    }
+}
